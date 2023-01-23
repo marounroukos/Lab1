c82c1ef447682efad0f92b83809d0dd907c27a9e
“marounroukos” <maroun.roukos@lau.edu>
2023-01-23 20:27:48 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -1,7 +1,9 @@
 public class Me
 {
         public static void main(String[] args){
-               System.out.print("Maroun Roukos");
+               System.out.println("Maroun Roukos");
                System.out.println("Computer Engineering");
+               System.out.println("2023");
+       
        }
 }
