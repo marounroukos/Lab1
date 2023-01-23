c866e0e6680acf94280cfe55e5e82b1dcb364aa2
“marounroukos” <maroun.roukos@lau.edu>
2023-01-23 20:10:58 +0200

    Maroun Roukos Computer Engineering

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me
+{
+        public static void main(String[] args){
+               System.out.print("Maroun Roukos");
+               System.out.println("Computer Engineering");
+       }
+}
